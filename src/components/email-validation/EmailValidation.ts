1da37d5ef992c0da4651571620645d745ed51486
bayodes <bayode.o.olaoye@gmail.com>
2022-06-07 15:35:42 -0400

    email validation progress

---
+++ src/components/email-validation/EmailValidation.ts
@@ -0,0 +1,21 @@
+export const isValidEmail: (email: String) => boolean = function (
+  email: String
+): boolean {
+  let lastAtPosition = email.lastIndexOf("@");
+  let lastDotPosition = email.lastIndexOf(".");
+
+  if (
+    !(
+      lastAtPosition < lastDotPosition &&
+      lastAtPosition > 0 &&
+      email.indexOf("@@") == -1 &&
+      lastDotPosition > 2 &&
+      email.length - lastDotPosition > 2
+    )
+  ) {
+    console.log("Sorry not an email");
+    return false;
+  } else {
+    return true;
+  }
+};
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,28 +1,63 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { apiLogin } from '../../remote/e-commerce-api/authService';
-import { useNavigate } from 'react-router-dom';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { apiLogin } from "../../remote/e-commerce-api/authService";
+import { useNavigate } from "react-router-dom";
+import { isValidEmail } from "../email-validation/EmailValidation";
+import "./Login.css";
 
 const theme = createTheme();
 
 export default function Login() {
   const navigate = useNavigate();
+  let email;
+  let checkEmail;
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    const response = await apiLogin(`${data.get('email')}`, `${data.get('password')}`);
-    if (response.status >= 200 && response.status < 300) navigate('/')
+
+    // let emailString = new String(data.get("email"));
+    // let passwordString = new String(data.get("password"));
+    // let lastAtPosition = emailString.lastIndexOf("@");
+    // let lastDotPosition = emailString.lastIndexOf(".");
+
+    // if (
+    //   !(
+    //     lastAtPosition < lastDotPosition &&
+    //     lastAtPosition > 0 &&
+    //     emailString.indexOf("@@") == -1 &&
+    //     lastDotPosition > 2 &&
+    //     emailString.length - lastDotPosition > 2
+    //   )
+    // ) {
+    //   console.log("Sorry not an email");
+    //   setValidEmail(false);
+    // } else {
+    //   console.log("valid email address");
+    //   const response = await apiLogin(
+    //     `${data.get("email")}`,
+    //     `${data.get("password")}`
+    //   );
+    //   if (response.status >= 200 && response.status < 300) navigate("/");
+    // }
+    console.log("valid email address");
+    const response = await apiLogin(
+      `${data.get("email")}`,
+      `${data.get("password")}`
+    );
+    email = new String(data.get("email"));
+    checkEmail = isValidEmail(email);
+    if (response.status >= 200 && response.status < 300) navigate("/");
   };
 
   return (
@@ -32,18 +67,28 @@ export default function Login() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            {checkEmail ? (
+              <></>
+            ) : (
+              <p className="invalid-email">Incorrect Email Format</p>
+            )}
             <TextField
               margin="normal"
               required
@@ -72,6 +117,7 @@ export default function Login() {
             >
               Sign In
             </Button>
+
             <Grid container>
               <Grid item>
                 <Link href="register" variant="body2">

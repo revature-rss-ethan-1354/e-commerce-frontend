eb9d8747704018a276acf510e2cab9fe0778652b
S1m1ster <darrylrobles14@gmail.com>
2022-06-13 16:00:36 -0400

    added input blocker fields in checkout

--- src/components/checkout-validation/ExpirationDateCardValidation.ts
+++ src/components/checkout-validation/ExpirationDateCardValidation.ts
@@ -15,7 +15,10 @@ export let isValidExpirationDate: (expirationDate: String) => String = function 
         return "Expiration date must not be empty";
     } else if (expirationMonthNumber > 12 || expirationMonthNumber < 1) {
         return "Invalid month";
-    } 
+    }
+    if(expirationDate.length > 4){
+        return "Invalid date format";
+    }
 
     if (expirationYearNumber == (currentYear - 2000)){
         if (expirationMonthNumber >= currentMonth) {
